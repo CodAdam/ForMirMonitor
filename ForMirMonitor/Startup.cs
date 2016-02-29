d198b13f610b0bac525a091ba7a3a7fb5481161d
Kailin Yang <Kailin Yang>
2016-02-29 23:48:26 +0800

    Second Push

---
+++ ForMirMonitor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForMirMonitor.Startup))]
+namespace ForMirMonitor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
