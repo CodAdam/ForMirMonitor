a1f20750b94ab1024cad12f72566037f20563360
CodAdam <1910347321@qq.com>
2016-03-02 18:32:00 +0800

    invalied

--- ForMirMonitor/Controllers/HomeController.cs
+++ ForMirMonitor/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace ForMirMonitor.Controllers
 {
@@ -14,6 +17,8 @@ namespace ForMirMonitor.Controllers
         }
 
         public ActionResult test() {
+
+            
             return View();
         }
 
@@ -29,6 +34,43 @@ namespace ForMirMonitor.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public void DownLoad(string strName, string strPath)
+        {
+            string fileName = strName;//客户端保存的文件名
+            string filePath = Server.MapPath(strPath);//路径
+
+            /*string strResult = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strPath);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream streamReceive = response.GetResponseStream();
+            Encoding encoding = Encoding.GetEncoding("gb2312");
+            StreamReader streamReader = new StreamReader(streamReceive, encoding);
+            strResult = streamReader.ReadToEnd();
+            div1.InnerText = strResult;*/
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strPath);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream fs = response.GetResponseStream(); 
+            /*Encoding encoding = Encoding.GetEncoding("gb2312");
+            StreamReader streamReader = new StreamReader(streamReceive, encoding);
+            strResult = streamReader.ReadToEnd();
+            div1.InnerText = strResult;
+
+            //以字符流的形式下载文件
+            FileStream fs = new FileStream(filePath, FileMode.Open);*/
+            byte[] bytes = new byte[(int)fs.Length];
+            fs.Read(bytes, 0, bytes.Length);
+            fs.Close();
+            Response.ContentType = "application/octet-stream";
+            //通知浏览器下载文件而不是打开
+            Response.AddHeader("Content-Disposition", "attachment;  filename=" + HttpUtility.UrlEncode(fileName, System.Text.Encoding.UTF8));
+            Response.BinaryWrite(bytes);
+            Response.Flush();
+            Response.End();
         }
     }
 }
