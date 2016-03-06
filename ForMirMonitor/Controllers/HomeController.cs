11b403fe5ea4d63709058b1d33704d5b04be6486
Kailin Yang <Kailin Yang>
2016-03-06 17:29:13 +0800

    service.common.email

--- ForMirMonitor/Controllers/HomeController.cs
+++ ForMirMonitor/Controllers/HomeController.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
 using System.Net;
-using System.Text;
+using System.Net.Mail;
+using FMM.Service.Common;
 
 namespace ForMirMonitor.Controllers
 {
@@ -41,41 +40,29 @@ namespace ForMirMonitor.Controllers
             return View("Index");
         }
 
+        public void sendEmail() {
+            try
+            {
+                string seFrom = "ForMirMonitor@163.com"; 
+                string seTo = "447173174@qq.com";
+                SendEmail se = new SendEmail(seTo, seFrom, "From ForMirMoniter", "Dear "+ seTo+":\n 以下是您的订阅信息，如有附件请注意查收。\n祝您愉快~");
+                string filePath = Request.PhysicalApplicationPath + "Download\\Commit_STATModel.xlsx";
+                se.Attachments(filePath);
+                se.SetSmtp("formirmonit0r", "smtp.163.com"); //必须在所有参数设置完后调用此方法  
+            }
+
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+            
+        }
+
         public void DownLoad(string strName, string strPath)
         {
-            string fileName = strName;//客户端保存的文件名
-            string filePath = Server.MapPath(strPath);//路径
 
-            /*string strResult = "";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strPath);
-            request.Method = "GET";
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream streamReceive = response.GetResponseStream();
-            Encoding encoding = Encoding.GetEncoding("gb2312");
-            StreamReader streamReader = new StreamReader(streamReceive, encoding);
-            strResult = streamReader.ReadToEnd();
-            div1.InnerText = strResult;*/
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strPath);
-            request.Method = "GET";
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream fs = response.GetResponseStream(); 
-            /*Encoding encoding = Encoding.GetEncoding("gb2312");
-            StreamReader streamReader = new StreamReader(streamReceive, encoding);
-            strResult = streamReader.ReadToEnd();
-            div1.InnerText = strResult;
-
-            //以字符流的形式下载文件
-            FileStream fs = new FileStream(filePath, FileMode.Open);*/
-            byte[] bytes = new byte[(int)fs.Length];
-            fs.Read(bytes, 0, bytes.Length);
-            fs.Close();
-            Response.ContentType = "application/octet-stream";
-            //通知浏览器下载文件而不是打开
-            Response.AddHeader("Content-Disposition", "attachment;  filename=" + HttpUtility.UrlEncode(fileName, System.Text.Encoding.UTF8));
-            Response.BinaryWrite(bytes);
-            Response.Flush();
-            Response.End();
         }
     }
 }
